bccd32024d59d275c5f0f5eecdeeba34545fe061
Arturo <arturo.juarez@globaloxs.com>
2021-03-11 15:34:26 -0600

    .

--- refugio/apps/mascota/models.py
+++ refugio/apps/mascota/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Mascota(models.Model):
+    # Django genera pk default si no tenemos una
+    folio = models.CharField(primary_key=True, max_length=100)
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
